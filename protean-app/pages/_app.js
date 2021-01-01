adf9334f32cf3f547e436444be2f0e0e9f4b705f
TheDolentCity <nardogamedev@outlook.com>
2021-01-01 14:18:45 -0800

    Summary: Added minimum width config Detailed: Added minimum width (and height) with 12 levels of detail to tailwind config. Then added this to the main page panels so that they work well with flex.

--- protean-app/pages/_app.js
+++ protean-app/pages/_app.js
@@ -10,14 +10,14 @@ function MyApp({ Component, pageProps }) {
         <Header
           title="Protean RPG">
         </Header>
-        <div className="max-h-screen whitespace-nowrap divide-x divide-gray-300 dark:divide-gray-700">
-          <div className="inline-block w-2/12 mt-14 max-h-full whitespace-normal overflow-y-auto p-8">
+        <div className="flex max-h-screen whitespace-nowrap divide-x divide-gray-300 dark:divide-gray-700">
+          <div className="flex-none min-w-2/12 mt-14 max-h-full whitespace-normal overflow-y-auto p-8">
             <BookNavigation></BookNavigation>
           </div>
-          <div className="inline-block w-4/12 mt-14 max-h-full whitespace-normal overflow-y-auto p-8">
+          <div className="flex-grow min-w-6/12 mt-14 max-h-full whitespace-normal overflow-y-auto p-8">
             <Component {...pageProps} />
           </div>
-          <div className="inline-block w-4/12 mt-14 max-h-full whitespace-normal overflow-y-auto">
+          <div className="flex-auto min-w-4/12 mt-14 max-h-full whitespace-normal overflow-y-auto">
             <Sheet></Sheet>
           </div>
         </div>
