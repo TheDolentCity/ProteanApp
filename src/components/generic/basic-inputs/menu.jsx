314ba5a8374bfa71753569feb888bee87d888271
Dante Nardo <nardogamedev@outlook.com>
2021-07-12 21:38:42 -0700

    Added CSS that capped the width and removed the unnecessary relative value

--- src/components/generic/basic-inputs/menu.jsx
+++ src/components/generic/basic-inputs/menu.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 
-export function MenuContainer({ style, children }) {
+export function MenuContainer({ className, style, children }) {
 	return (
-		<div className="fixed flex flex-none flex-col min-w-48 rounded shadow-xl bg-white border border-gray-300 dark:border dark:border-gray-600 divide-y divide-gray-300 dark:bg-gray-700 dark:divide-gray-600" style={style}>
+		<div
+			className={"fixed flex flex-none flex-col min-w-48 rounded shadow-xl bg-white border border-gray-300 dark:border dark:border-gray-600 divide-y divide-gray-300 dark:bg-gray-700 dark:divide-gray-600 " + className}
+			style={style}>
 			{children}
 		</div>
 	);
